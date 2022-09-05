30318cad803ee97d2a05ed72a9dbcfaf60259d2f
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-05 09:29:53 -0300

    update firstDigit.js

--- src/firstDigit.js
+++ src/firstDigit.js
@@ -1,10 +1,3 @@
-// For inputString = "var_1__Int", the output should be
-// solution(inputString) = '1';
-// For inputString = "q2q-q", the output should be
-// solution(inputString) = '2';
-// For inputString = "0ss", the output should be
-// solution(inputString) = '0'.
-
 function solution(inputString) {
     let firstDigit = inputString.match(/\d/);
     return firstDigit ? firstDigit[0] : '0';
